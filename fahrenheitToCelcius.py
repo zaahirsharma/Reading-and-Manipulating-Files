67e40a4e3a64c607cdbd93f1a85894237a5b8432
zaahirsharma <sharma.zaahir@gmail.com>
2024-08-29 13:47:21 -0500

    Update fahrenheitToCelcius.py

--- fahrenheitToCelcius.py
+++ fahrenheitToCelcius.py
@@ -1,4 +1,6 @@
-'''
+# The Celcius.dat.txt has not been uploaded but others can create their own
+# with each Celcius temperature on a new line and use this code for conversion.
+
 with open("Celcius.dat.txt", "r") as myFile:
     newFile = open("Fahrenheit.dat", "w")
     line = myFile.readline()
@@ -7,6 +9,4 @@ with open("Celcius.dat.txt", "r") as myFile:
         num = (num*9/5)+32
         newFile.write(str(num)+"\n")
         line = myFile.readline()
-'''
-# The Celcius.dat.txt has not been uploaded but others can create their own
-# with each Celcius temperature on a new line and use this code for conversion.
+
